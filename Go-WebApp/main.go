40e604f2f2dfdd991ed6b5ab75f2c09e45b60d12
ChamathKB <amsmckbattanayaka@std.appsc.sab.ac.lk>
2022-11-12 18:12:01 +0530

    adding go-routines

--- Go-WebApp/main.go
+++ Go-WebApp/main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 )
 
-var store = NewURLStore("store.gob")
+var (
+	listenAddr = flag.String("http", ":3000", "http listen address")
+	dataFile   = flag.String("file", "store.gob", "data store file name")
+	hostname   = flag.String("host", "localhost", "http host name")
+)
+
+var store *URLStore
 
 func main() {
+	flag.Parse()
+	store = NewURLStore(*dataFile)
 	http.HandleFunc("/", Redirect)
 	http.HandleFunc("/add", Add)
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(*listenAddr, nil)
 }
 
 func Redirect(w http.ResponseWriter, r *http.Request) {
